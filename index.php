a846010aac6002b7af31586d1e9d4da5dbc1df91
Sergiy <mradvirus@gmail.com>
2014-10-27 20:20:55 +0200

    new file

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HTML5 Login</title>
+    <link rel="stylesheet" href="CSS/normalize.css">
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+<section class="loginform cf">
+    <form name="login" action="handler.php" method="post">
+        <ul>
+            <li>
+                <label for="usermail">User</label>
+                <input type="text" name="login" placeholder="your login" required>
+            </li>
+            <li>
+                <label for="password">Password</label>
+                <input type="password" name="password" placeholder="password" required></li>
+            <li>
+                <input type="submit" value="Login">
+            </li>
+        </ul>
+    </form>
+</section>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADVIRUS
+ * Date: 26.10.2014
+ * Time: 11:53
+ */
+
